9850176125b29450eb66ab0576cf50a8493d652f
Margarita <m.denisik@gmail.com>
2016-02-15 18:47:27 +0300

    2

--- js/server.js
+++ js/server.js
@@ -1,22 +1,9 @@
 var http = require('http');
 var url = require('url');
 var getIp = require('./getIp');
-var historyMod = require('./history');
 
-
-var history = new historyMod.History();
-var toBeResponded = [];
 var ip = getIp();
-
 var startTime = curentDateTime();
-
-var handlerMap = {
-    "GET": getHandler,
-    "OPTIONS": optionsHandler,
-    "DELETE": deleteHandler,
-    "POST": postHandler,
-    "PUT": putHandler
-};
 
 var errStatusMap = {
     "Deleting non-existent message": 422,
@@ -33,12 +20,11 @@ var errStatusMap = {
 var server = http.createServer(function(req, res) {
     console.log('method: ' + req.method + ", " + req.url);
 
-    var handler = handlerMap[req.method];
-    if (handler == undefined) {
+    if (req.method != "GET") {
         responseWith(res, Error("Unsuported http request"));
     }
 
-    handlerMap[req.method](req, res, function(err) {
+    getHandler(req, res, function(err) {
         responseWith(res, err);
     });
 });
@@ -78,107 +64,11 @@ function getHandler(req, res, continueWith) {
     if (req.url == "/") {
         continueWith({
             status: "Running",
-            startTime: startTime,
-            curentToken: history.getToken()
+            startTime: startTime
         });
         return;
     }
 
-    if (urlToken == undefined) {
-        continueWith(Error("Bad Request"));
-        return;
-    }
-
-    history.getMessages(urlToken, function(answer, error) {
-        if (error !== undefined) {
-            continueWith(error);
-            return;
-        }
-        if (answer !== undefined) {
-            var body = {
-                token: history.getToken(),
-                messages: answer
-            };
-            continueWith(body);
-            return;
-        }
-        remaineWait(req, res, continueWith);
-    });
-}
-
-function postHandler(req, res, continueWith) {
-    awaitBody(req, function(message) {
-        history.addMessage(message, function(err) {
-            if (err) {
-                continueWith(err);
-            } else {
-                respondAll();
-                continueWith();
-            }
-        });
-    });
-}
-
-function optionsHandler(req, res, continueWith) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    continueWith();
-}
-
-function putHandler(req, res, continueWith) {
-    awaitBody(req, function(message) {
-        history.editMessage(message, function(err) {
-            if (err) {
-                continueWith(err);
-            } else {
-                respondAll();
-                continueWith();
-            }
-        });
-    });
-}
-
-function deleteHandler(req, res, continueWith) {
-    awaitBody(req, function(message) {
-        history.deleteMessage(message, function(err) {
-            if (err) {
-                continueWith(err);
-            } else {
-                respondAll();
-                continueWith();
-            }
-        });
-    });
-}
-
-function remaineWait(req, res, continueWith) {
-    toBeResponded.push({
-        request: req,
-        response: res,
-        continueWith: continueWith
-    });
-}
-
-function respondAll() {
-    toBeResponded.forEach(function(waiter) {
-        getHandler(waiter.request, waiter.response, waiter.continueWith);
-    });
-    toBeResponded = [];
-}
-
-function awaitBody(req, handler) {
-    var reqBody = '';
-    req.on('data', function(data) {
-        reqBody += data.toString();
-    });
-
-    req.on('end', function() {
-        handler(JSON.parse(reqBody));
-    });
-}
-
-function getUrlToken(u) {
-    var parts = url.parse(u, true);
-    return parts.query.token;
 }
 
 function getHourMinutes(utcNumberDate) {
